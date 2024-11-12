352a5180b66598b8b1992eb45f0110e585d8857b
dualkeyboards <dualkeyboards@outlook.com>
2024-11-13 02:15:41 +0700

    minor adjustment

--- _includes/interrupt.py
+++ _includes/interrupt.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 def interrupt_handler(status_bar):
     def signal_handler(sig, frame):
-        logger.info(f"\033[47;30m CANCELLED       EXITING\n\033[0m")
+        logger.info(f"\033[41;30m CANCELLED       EXITING \033[0m \n")
         status_bar.clear()
         os._exit(1)
 
